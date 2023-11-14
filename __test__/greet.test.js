26d4a74a3e8a8dc4baa070d1c19062262ce30b54
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2023-11-14 13:56:35 +0100

    add : test2

--- __test__/greet.test.js
+++ __test__/greet.test.js
@@ -5,6 +5,27 @@ test('should return Hello, Brandon. when name is Brandon', () => {
   expect(greet('Brandon')).toBe('Hello, Brandon.')
 })
 
+/*
+Test2a
+*/
+test('should return hello my friend when name is null', () => {
+  expect(greet(null)).toBe('Hello, my friend.')
+})
+
+/*
+Test2b
+*/
+test('should return hello my friend when name is undefined', () => {
+  expect(greet(undefined)).toBe('Hello, my friend.')
+})
+
+/*
+Test2c
+*/
+test('should return hello my friend when name is empty', () => {
+  expect(greet(' ')).toBe('Hello, my friend.')
+})
+
 test('should return HELLO, BRANDON! when name is BRANDON', () => {
   expect(greet('BRANDON')).toBe('HELLO, BRANDON!')
 })
